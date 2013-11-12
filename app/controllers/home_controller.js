57cdd30b90be4533251a4db2aa574ac068bac7d6
namvu80ap <thanhcong>
2013-11-12 23:59:35 +0900

    Add upload file

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -1,3 +1,38 @@
 action('index', function () {
 	render( {title: "Posts index" } );
 });
+
+/**  
+ *   
+ * show upload form  
+ */  
+action('upload_form', function () {   
+ render({  
+  title: "upload_controller#upload_form"  
+ });  
+});  
+
+/**  
+ *   
+ * save uploaded file, error handling
+ */  
+action('upload_file', function () {   
+
+  var fs = require('fs');  
+
+  this.file_name = req.body.file_name;  
+  this.uploaded_file_tmp = req.files.file.path;  
+  this.uploaded_file_type = req.files.file.type;  
+  this.uploaded_file_name = req.files.file.name;  
+  var new_file = "/home/yourhomedir/compound_test/uploaded_files/"+this.uploaded_file_name;  
+
+  fs.rename( this.uploaded_file_tmp, new_file, function(err){  
+   if(err) console.log(err);  
+    console.log("moved");  
+  });  
+
+  console.log(this.uploaded_file_type);  
+    render({  
+      title: "upload_controller#upload_file"  
+  });  
+});  
--- config/routes.js
+++ config/routes.js
@@ -9,4 +9,8 @@ exports.routes = function (map) {
     // feel free to remove generic routes
     map.all(':controller/:action');
     map.all(':controller/:action/:id');
+    
+    //Update
+    map.get('upload_form', 'upload#upload_form');  
+    map.post('upload_file', 'upload#upload_file');
 };
