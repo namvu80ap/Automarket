60655709debea59c744441828cfc86f24e4411db
vohoal_nam <vohoal_nam@10.10.6.161>
2013-11-22 18:27:27 +0900

    add token

--- app/controllers/upload_controller.js
+++ app/controllers/upload_controller.js
@@ -34,48 +34,53 @@ action('upload_file', function () {
   
   console.log( "================" );
   this.file_name = req.body.file_name;  
-  this.uploaded_file_tmp = req.files.file.path;  
-  this.uploaded_file_type = req.files.file.type;  
-  this.uploaded_file_name = req.files.file.name;  
-  var new_file = "./public/"+this.uploaded_file_name;
+  this.uploaded_file_tmp = req.files.file.path;
+  this.uploaded_file_type = req.files.file.type;
+  this.uploaded_file_name = req.files.file.name;
+  //var new_file = "./public/"+this.uploaded_file_name;
   
   console.log("fdsdfsfdsf");
   // Server-side applications use both the API key and secret.
   var client = new Dropbox.Client({
-      key: "gzzlxacfelfpf0u",
-      secret: "ssx3n6x2ubqjs5w"
+	  key: 'gzzlxacfelfpf0u',
+	  secret: 'ssx3n6x2ubqjs5w',
+	  token: 'TrGHfPHWkcUAAAAAAAAAAbL08JTvrtvOek-cq7vzMLjQ0YMV0hNZQ-_Dh3JJfG-0',
+	  uid: '181841728'
   });
-  console.log("fdsdfsfdsf!!!!!!!!!!");
+  
   client.authDriver(new Dropbox.AuthDriver.NodeServer(8191));
-  console.log("fdsdfsfdsf********");
+  
   client.authenticate( false ,function(error, clientA) {
   	  if (error) {
-  	    // Replace with a call to your own error-handling code.
-  	    //
-  	    // Don't forget to return from the callback, so you don't execute the code
-  	    // that assumes everything went well.
   	   console.log(error);
   	  }
   	  
-	  	console.log('connected...');
-	    console.log('token ', clientA);       // THE_TOKEN
-	    //console.log('secret', clientA); // THE_TOKEN_SECRET
+  	  console.log("===============");
+  	  console.log(clientA.credentials());
+  	  console.log("===============");
+  	  
 	    fs.readFile( req.files.file.path , function read( err, data ){
 	    	if( err )console.log( err );
 	    	console.log(data);
-		    client.writeFile("hello_world3.jpg", data , function(error, stat) {
+		    client.writeFile("hello_world4.jpg", data , function(error, stat) {
 		    	console.log("do copy");
 		    	
 		    	console.log(error);
 				console.log("File saved as revision ");
 				console.log(stat);
 				
-				client.makeUrl( "hello_world2.jpg", { downloadHack:true }, function( err, url ){
+				client.makeUrl( "hello_world4.jpg", { downloadHack:true }, function( err, url ){
 			    	if(err) console.log(err);
 			    	console.log(url);
-			    	picArr[picArr.length] = { url: url.url };
+			    	//picArr[picArr.length] = { url: url.url };
+			    	console.log(picArr);
+			    	if( picArr.length > 0 ){
+			    		picArr[picArr.length] = "{ url : " + url.url + "}";
+					}
+			    	else{
+			    		picArr.push( "[{ url : " + url.url + "}]" );
+			    	}
 			    	Car.find(id, function( err, car ){
-			    		//car.pics = picArr;
 			    		car.updateAttributes( { pics : picArr } , function(err){ if(err) console.log(err); } );
 			    	})
 			    } );
